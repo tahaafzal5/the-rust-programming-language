1782f9e93a44a5a7403a4da8c1e2cf2f7ae2f01a
Taha Afzal <tahaafzal5@hotmail.com>
2024-02-26 21:26:44 -0600

    Add ch8 exercises cargo projects

---
+++ projects/ch8/mean-median-mode/src/main.rs
@@ -0,0 +1,6 @@
+// Exercise: Given a list of integers, use a vector and return the median (when sorted, the value in the middle position)
+// and mode (the value that occurs most often) of the list.
+
+fn main() {
+    println!("Hello, world!");
+}
