c903c36d5e8da3dee4f269db9750622375952cdc
Taha Afzal <tahaafzal5@hotmail.com>
2024-03-23 12:49:58 -0500

    Initial commit for minigrep

---
+++ projects/ch12/minigrep/src/main.rs
@@ -0,0 +1,35 @@
+use std::{env, fs, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Error parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {} in {}", config.query, config.file_path);
+
+    let file_contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file.");
+
+    println!("file_contents: \n{}", file_contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
