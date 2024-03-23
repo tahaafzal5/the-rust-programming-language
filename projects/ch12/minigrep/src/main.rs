45e2913760d890790572a949dd25870fb4ced254
Taha Afzal <tahaafzal5@hotmail.com>
2024-03-23 13:21:54 -0500

    Move stuff to lib.rs

--- projects/ch12/minigrep/src/main.rs
+++ projects/ch12/minigrep/src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,34 +10,8 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(error) = run(config) {
+    if let Err(error) = minigrep::run(config) {
         println!("Application error: {error}");
         process::exit(1);
     };
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let file_contents = fs::read_to_string(config.file_path)?;
-
-    println!("file_contents: \n{}", file_contents);
-
-    Ok(())
 }
